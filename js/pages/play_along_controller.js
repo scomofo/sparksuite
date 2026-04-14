75b8f139d371b986c1ee3ad2cefa4b75e06594d8
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-14 10:57:53 -0600

    Default play-along bootstrap target to window

--- js/pages/play_along_controller.js
+++ js/pages/play_along_controller.js
@@ -1,5 +1,3 @@
 // ===== SparkSuite: Play Along Controller =====
 // Glue layer connecting play-along page UI to SparkCore play-along pipeline.
-(function() {
-  SparkPlayAlongActionService.bootstrapController();
-})();
+SparkPlayAlongActionService.bootstrapController();
