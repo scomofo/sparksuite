3aef9068363329f237021b81b53b8c92ae3f690b
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-14 13:02:29 -0600

    Route piano export button through shared action

--- js/instruments/piano/pages/tools.js
+++ js/instruments/piano/pages/tools.js
@@ -242,7 +242,7 @@ function settingsTab() {
 
   html += '<h3 style="margin-top:20px">Data</h3>';
   html += '<div class="setting-row">';
-  html += '<button class="btn" onclick="exportState()" style="background:#F59E0B;color:#fff">Export JSON</button>';
+  html += '<button class="btn" onclick="act(\'exportProgress\')" style="background:#F59E0B;color:#fff">Export JSON</button>';
   html += '</div>';
   html += '<div class="setting-row">';
   html += '<button class="btn btn-danger" onclick="if(confirm(\'Reset all progress?\'))act(\'reset\')">Reset Progress</button>';
