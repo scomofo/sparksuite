4b76e2576e4ff5d075e43a9d1fb9f315c46f16d0
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-25 00:42:14 -0600

    fix: remove showroom profile template fallbacks

---
+++ tests/test_showroom_template_guardrails.js
@@ -0,0 +1,42 @@
+var assert = require("assert");
+var fs = require("fs");
+var path = require("path");
+
+function load(file) {
+  return fs.readFileSync(path.join(__dirname, "..", file), "utf8");
+}
+
+function assertAbsent(source, token, file) {
+  assert.strictEqual(
+    source.indexOf(token),
+    -1,
+    file + " should not include template token: " + token
+  );
+}
+
+console.log("\n--- Showroom Template Guardrails ---");
+
+var showroomSource = load("js/showroom/spark-showroom.js");
+var legacyProfileSource = load("js/meta/profile.js");
+
+[
+  "Alex Chen",
+  "Virtuoso",
+  "Neon Horizon",
+  "Midnight Ember Jam",
+  "Guitar Fundamentals",
+  "Module 1: Getting Started",
+  "Rhythm King",
+  "Night Owl"
+].forEach(function(token) {
+  assertAbsent(showroomSource, token, "js/showroom/spark-showroom.js");
+});
+
+assertAbsent(legacyProfileSource, "Player Profile</b></div><div>Level:", "js/meta/profile.js");
+
+assert.ok(showroomSource.indexOf("No badges unlocked yet.") >= 0);
+assert.ok(showroomSource.indexOf("No songs loaded for this instrument yet.") >= 0);
+assert.ok(showroomSource.indexOf("No completed session yet") >= 0);
+assert.ok(showroomSource.indexOf("No performance loaded") >= 0);
+
+console.log("  PASS: active profile/showroom surfaces are guarded against template fallbacks");
