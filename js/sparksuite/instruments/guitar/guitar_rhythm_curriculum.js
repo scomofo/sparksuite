1b812ee8fd0803c27255406eac4866a029788192
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-27 13:54:50 -0600

    Route rhythm curriculum through module chart selection

--- js/sparksuite/instruments/guitar/guitar_rhythm_curriculum.js
+++ js/sparksuite/instruments/guitar/guitar_rhythm_curriculum.js
@@ -1,6 +1,9 @@
 (function() {
   window.SparkGuitarRhythmCurriculum = {
     selectChartId: function(context) {
+      if (window.SparkGuitarModule && typeof window.SparkGuitarModule.selectChartId === "function") {
+        return window.SparkGuitarModule.selectChartId(context);
+      }
       context = context || {};
       if (context && context.segment && context.segment.meta && context.segment.meta.accuracy < 75) {
         return "power_chords_01";
