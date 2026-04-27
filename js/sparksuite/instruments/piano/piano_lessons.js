067d8bc2fa6135677235a399b231b0b2ba00d1e1
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-27 14:03:15 -0600

    Add piano lessons

---
+++ js/sparksuite/instruments/piano/piano_lessons.js
@@ -0,0 +1,14 @@
+(function(){
+  window.SparkPianoLessons = [
+    {id:"lesson_pno_01",level:1,title:"Orientation",skill:"pno_orientation"},
+    {id:"lesson_pno_02",level:1,title:"Five finger",skill:"pno_five_finger"},
+    {id:"lesson_pno_03",level:1,title:"Pulse",skill:"pno_quarter_pulse"},
+    {id:"lesson_pno_04",level:2,title:"C major",skill:"pno_c_major"},
+    {id:"lesson_pno_05",level:2,title:"G major",skill:"pno_g_major"},
+    {id:"lesson_pno_06",level:2,title:"Switch",skill:"pno_switch"},
+    {id:"lesson_pno_07",level:3,title:"Arpeggio",skill:"pno_arpeggio"},
+    {id:"lesson_pno_08",level:3,title:"Scale",skill:"pno_scale_c"},
+    {id:"lesson_pno_09",level:4,title:"Melody",skill:"pno_melody"},
+    {id:"lesson_pno_10",level:4,title:"Performance",skill:"pno_performance"}
+  ];
+})();
