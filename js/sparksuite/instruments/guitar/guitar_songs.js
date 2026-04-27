6e6a032aeabf22827f17454df131019be52db028
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-27 13:36:21 -0600

    Add guitar Step 2 songs

---
+++ js/sparksuite/instruments/guitar/guitar_songs.js
@@ -0,0 +1,10 @@
+(function() {
+  window.SparkGuitarSongs = [
+    { id: "gtr_song_01", title: "Two Chord Jam", chords: ["Em", "G"], bpm: 70 },
+    { id: "gtr_song_02", title: "Four Chord Loop", chords: ["G", "D", "Em", "C"], bpm: 78 },
+    { id: "gtr_song_03", title: "Slow Strum Ballad", chords: ["C", "G", "Am", "F"], bpm: 68 },
+    { id: "gtr_song_04", title: "Pop Progression", chords: ["D", "A", "Bm", "G"], bpm: 90 },
+    { id: "gtr_song_05", title: "Island Style", chords: ["C", "Am", "F", "G"], bpm: 82 },
+    { id: "gtr_song_06", title: "Fingerpick Flow", chords: ["Em", "C", "G", "D"], bpm: 72 }
+  ];
+})();
