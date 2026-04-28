f42e6685376f6e0b19dca44af8023cbcf8d8ce69
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-27 20:39:01 -0600

    Add missing ukulele runtime adapter placeholder

---
+++ js/sparksuite/instruments/ukulele/ukulele_runtime_adapter.js
@@ -0,0 +1,7 @@
+(function(){
+  if(!window.SparkUkuleleRuntimeAdapter){
+    window.SparkUkuleleRuntimeAdapter=function(){};
+    SparkUkuleleRuntimeAdapter.prototype.getLaneCount=function(){return 4;};
+    SparkUkuleleRuntimeAdapter.prototype.getLaneLabels=function(){return ["String1","String2","String3","String4"];};
+  }
+})();
