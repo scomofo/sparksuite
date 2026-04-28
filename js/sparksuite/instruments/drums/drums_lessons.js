da59f8fde6007413a546bf7f6c4e4599914f0e6f
scomofo <159295352+scomofo@users.noreply.github.com>
2026-04-27 20:25:28 -0600

    Add DrumSpark lessons

---
+++ js/sparksuite/instruments/drums/drums_lessons.js
@@ -0,0 +1,10 @@
+(function(){
+  window.SparkDrumsLessons=[
+    {id:"lesson_drums_kit_orientation_01",title:"Meet the Kit",skill:"kit_orientation"},
+    {id:"lesson_drums_count_01",title:"Count 1-2-3-4",skill:"counting_quarters"},
+    {id:"lesson_drums_single_01",title:"Single Strokes",skill:"single_strokes"},
+    {id:"lesson_drums_click_01",title:"Lock to Click",skill:"metronome_lock"},
+    {id:"lesson_drums_kick_01",title:"Kick Control",skill:"kick_control"},
+    {id:"lesson_drums_backbeat_01",title:"First Backbeat",skill:"basic_backbeat"}
+  ];
+})();
